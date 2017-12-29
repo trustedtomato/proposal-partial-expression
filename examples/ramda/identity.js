50239fe725628b806e6337d80337533a2c7caeaa
Halasi Tamás <trusted.tomato@gmail.com>
2017-12-29 13:05:38 +0100

    Add Ramda's identity.js

---
+++ examples/ramda/identity.js
@@ -0,0 +1,24 @@
+import _curry1 from './internal/_curry1';
+
+
+/**
+ * A function that does nothing but return the parameter supplied to it. Good
+ * as a default or placeholder function.
+ *
+ * @func
+ * @memberOf R
+ * @since v0.1.0
+ * @category Function
+ * @sig a -> a
+ * @param {*} x The value to return.
+ * @return {*} The input value, `x`.
+ * @example
+ *
+ *      R.identity(1); //=> 1
+ *
+ *      var obj = {};
+ *      R.identity(obj) === obj; //=> true
+ * @symb R.identity(a) = a
+ */
+var identity = _curry1(#?);
+export default identity;
