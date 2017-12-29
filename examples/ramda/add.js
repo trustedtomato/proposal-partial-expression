9af05606fb1ae214b1c19dd31e90367e9ac9df47
Halasi Tamás <trusted.tomato@gmail.com>
2017-12-29 12:43:47 +0100

    Add Ramda's add.js

---
+++ examples/ramda/add.js
@@ -0,0 +1,22 @@
+mport _curry2 from './internal/_curry2';
+
+
+/**
+ * Adds two values.
+ *
+ * @func
+ * @memberOf R
+ * @since v0.1.0
+ * @category Math
+ * @sig Number -> Number -> Number
+ * @param {Number} a
+ * @param {Number} b
+ * @return {Number}
+ * @see R.subtract
+ * @example
+ *
+ *      R.add(2, 3);       //=>  5
+ *      R.add(7)(10);      //=> 17
+ */
+var add = _curry2(#Number(?) + Number(?));
+export default add;
